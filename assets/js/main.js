798a7a3e5ef1e809aa18625318d6f0593c20a2a4
Renato Trench <retrench_er@hotmail.com>
2025-04-26 22:40:40 -0300

    concluido

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
-
+const pokemonContent = document.getElementById('pokemonContent')
 const maxRecords = 151
 const limit = 10
 let offset = 0;
@@ -19,15 +19,24 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+            <button class="pokeClick" type="button" data-number="${pokemon.number}">
+              Ver detalhes
+            </button>
         </li>
     `
+
 }
 
 function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+      const newHtml = pokemons.map(convertPokemonToLi).join('')
+      pokemonList.innerHTML += newHtml
+      
+      // Adicionar event listeners para os novos botões
+      document.querySelectorAll('.pokeClick').forEach(button => {
+          button.addEventListener('click', handleClick)
+      })
+  })
 }
 
 loadPokemonItens(offset, limit)
@@ -45,3 +54,36 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+function seeDetails(pokemon){
+  return`
+      <div class="detailsHeader">
+        <img class="detailPhoto" src="${pokemon.photo}"
+        alt="${pokemon.name}">
+      </div>
+      <div class="detailsContent">
+          <div class="abilities">
+          <h3>Abilities</h3>
+          <ol class="ability">
+          ${pokemon.abilities.map((ability) => `<li class="type ${ability}">${ability}</li>`).join('')}
+          </ol>
+          </div>
+          <div class="stats">
+          <h3>Stats</h3>
+          <ol class="stat">
+          ${pokemon.stats.map((stat) => `<li class="type ${stat.name}">${stat.name}: ${stat.value}</li>`).join('')}
+          </ol>
+          </div>
+      </div>
+  `
+
+}
+
+function handleClick(event) {
+  const pokemonNumber = event.target.getAttribute('data-number')
+  console.log(pokemonNumber);
+  pokeApi.getPokemonById(pokemonNumber).then((pokemon) => {
+    const detailsHtml = seeDetails(pokemon)
+    pokemonContent.innerHTML = '';
+    pokemonContent.innerHTML += detailsHtml
+  })}
